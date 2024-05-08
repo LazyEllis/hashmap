3aaabedf5b4475ce3e74a341dcbca9688dbaab1c
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2024-05-08 01:18:29 +0100

    refactor: Set some public properties to private
    This commit changes some public properties of the HashMap class to
    private. This is done to prevent the properties from being accessed
    directly from outside the class.
    
    - The `capacity` and `loadFactor` fields are now private.
    - The  `shrinkHashMap`, `growHashMap`, and `hash` methods are now
      private.

--- hashmap.js
+++ hashmap.js
@@ -8,34 +8,34 @@ class HashMap {
    * Create a new HashMap.
    */
   constructor() {
-    /**
-     * The initial capacity of the hashmap.
-     * @type {number}
-     */
-    this.capacity = 16;
-
-    /**
-     * The load factor threshold for resizing the hashmap.
-     * @type {number}
-     */
-    this.load_factor = 0.75;
-
     // Initialize each bucket in the hashmap with a linked list
-    for (let hash = 0; hash < this.capacity; hash++) {
+    for (let hash = 0; hash < this.#capacity; hash++) {
       this[hash] = new LinkedList();
     }
   }
 
   /**
+   * The initial capacity of the hashmap.
+   * @type {number}
+   */
+  #capacity = 16;
+
+  /**
+   * The load factor threshold for resizing the hashmap.
+   * @type {number}
+   */
+  #loadFactor = 0.75;
+
+  /**
    * Doubles the capacity of the hashmap and rehashes all key-value pairs.
    */
-  growHashMap() {
-    if (this.length() >= this.load_factor * this.capacity) {
+  #growHashMap() {
+    if (this.length() >= this.#loadFactor * this.#capacity) {
       const entries = this.entries();
-      this.capacity *= 2;
+      this.#capacity *= 2;
 
       // Create new buckets and rehash all entries
-      for (let hash = 0; hash < this.capacity; hash++) {
+      for (let hash = 0; hash < this.#capacity; hash++) {
         this[hash] = new LinkedList();
       }
 
@@ -48,15 +48,15 @@ class HashMap {
   /**
    * Halves the capacity of the hashmap and rehashes all key-value pairs.
    */
-  shrinkHashMap() {
-    if (this.capacity === 16) return;
+  #shrinkHashMap() {
+    if (this.#capacity === 16) return;
 
-    if (this.length() < this.load_factor * (this.capacity / 2)) {
+    if (this.length() < this.#loadFactor * (this.#capacity / 2)) {
       const entries = this.entries();
-      this.capacity /= 2;
+      this.#capacity /= 2;
 
       // Create new buckets and rehash all entries
-      for (let hash = 0; hash < this.capacity; hash++) {
+      for (let hash = 0; hash < this.#capacity; hash++) {
         this[hash] = new LinkedList();
       }
 
@@ -71,11 +71,11 @@ class HashMap {
    * @param {string} key - The key to hash.
    * @returns {number} - The computed hash code.
    */
-  hash(key) {
+  #hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
     }
     return hashCode;
   }
@@ -86,9 +86,9 @@ class HashMap {
    * @param {*} value - The value associated with the key.
    */
   set(key, value) {
-    const hashCode = this.hash(key);
+    const hashCode = this.#hash(key);
     this[hashCode].append(key, value);
-    this.growHashMap();
+    this.#growHashMap();
   }
 
   /**
@@ -97,7 +97,7 @@ class HashMap {
    * @returns {*} - The value associated with the key, or null if not found.
    */
   get(key) {
-    const hashCode = this.hash(key);
+    const hashCode = this.#hash(key);
     const node = this[hashCode].find(key);
     return node ? node.value : null;
   }
@@ -108,7 +108,7 @@ class HashMap {
    * @returns {boolean} - True if the key exists, false otherwise.
    */
   has(key) {
-    const hashCode = this.hash(key);
+    const hashCode = this.#hash(key);
     return this[hashCode].contains(key);
   }
 
@@ -117,9 +117,9 @@ class HashMap {
    * @param {string} key - The key to remove.
    */
   remove(key) {
-    const hashCode = this.hash(key);
+    const hashCode = this.#hash(key);
     this[hashCode].removeAt(key);
-    this.shrinkHashMap();
+    this.#shrinkHashMap();
   }
 
   /**
@@ -128,7 +128,7 @@ class HashMap {
    */
   length() {
     let length = 0;
-    for (let hash = 0; hash < this.capacity; hash++) {
+    for (let hash = 0; hash < this.#capacity; hash++) {
       length += this[hash].size();
     }
     return length;
@@ -138,8 +138,8 @@ class HashMap {
    * Clears all key-value pairs from the hashmap.
    */
   clear() {
-    this.capacity = 16;
-    for (let hash = 0; hash < this.capacity; hash++) {
+    this.#capacity = 16;
+    for (let hash = 0; hash < this.#capacity; hash++) {
       this[hash].head = null;
     }
   }
@@ -150,7 +150,7 @@ class HashMap {
    */
   keys() {
     let keys = [];
-    for (let hash = 0; hash < this.capacity; hash++) {
+    for (let hash = 0; hash < this.#capacity; hash++) {
       keys = keys.concat(this[hash].keysToArray());
     }
     return keys;
@@ -162,7 +162,7 @@ class HashMap {
    */
   values() {
     let values = [];
-    for (let hash = 0; hash < this.capacity; hash++) {
+    for (let hash = 0; hash < this.#capacity; hash++) {
       values = values.concat(this[hash].valuesToArray());
     }
     return values;
@@ -174,7 +174,7 @@ class HashMap {
    */
   entries() {
     let keyValues = [];
-    for (let hash = 0; hash < this.capacity; hash++) {
+    for (let hash = 0; hash < this.#capacity; hash++) {
       keyValues = keyValues.concat(this[hash].keyValuesToArray());
     }
     return keyValues;
